4b2dd971854cca7013d2998e6f3b79e416cba2c1
awaywiththespoons <becca.rose@raspberrypi.org>
2026-02-03 10:36:18 +0000

    Update game.py

--- en/code/editor-rpg-complete/game.py
+++ en/code/editor-rpg-complete/game.py
@@ -2,11 +2,11 @@
 inventory = []
 
 # Start the player in the Hall
-currentRoom = "Hall"
+currentRoom = 'Hall'
 
 def showInstructions():
     # Print a main menu and the commands
-    print("""
+    print('''
 Monster Game
 ========
 
@@ -16,19 +16,19 @@ Avoid the monsters!
 Commands:
 go [direction]
 get [item]
-""")
+''')
 
 
 def showStatus(currentRoom, inventory, rooms):
     # Print the player's current status
-    print("---------------------------")
-    print("You are in the " + currentRoom)
+    print('---------------------------')
+    print('You are in the ' + currentRoom)
     # Print the current inventory
-    print("Inventory : " + str(inventory))
+    print('Inventory : ' + str(inventory))
     # Print an item if there is one
-    if "item" in rooms[currentRoom]:
-        print("You see a " + rooms[currentRoom]["item"])
-    print("---------------------------")
+    if 'item' in rooms[currentRoom]:
+        print('You see a ' + rooms[currentRoom]['item'])
+    print('---------------------------')
     
     
 def run_game(currentRoom, inventory, rooms):
@@ -48,36 +48,36 @@ def run_game(currentRoom, inventory, rooms):
         # .split() breaks it up into an list array
         # e.g. typing 'go east' would give the list:
         # ['go','east']
-        move = ""
-        while move == "":
-            move = input(">")
+        move = ''
+        while move == '':
+            move = input('>')
 
         move = move.lower().split()
 
         # If they type 'go' first
-        if move[0] == "go":
+        if move[0] == 'go':
             # Check that they are allowed wherever they want to go
             if move[1] in rooms[currentRoom]:
                 # Set the current room to the new room
                 currentRoom = rooms[currentRoom][move[1]]
             else:
-                print("You can't go that way!")
+                print('You can't go that way!')
 
         # If they type 'get' first
-        if move[0] == "get":
+        if move[0] == 'get':
             # If the room contains an item, and the item is the one they want to get
-            if "item" in rooms[currentRoom] and move[1] == rooms[currentRoom]["item"]:
+            if 'item' in rooms[currentRoom] and move[1] == rooms[currentRoom]['item']:
                 # Add the item to their inventory
                 inventory.append(move[1])
-                print("You picked up the " + move[1])
+                print('You picked up the ' + move[1])
                 # Delete the item from the room
-                del rooms[currentRoom]["item"]
+                del rooms[currentRoom]['item']
             else:
                 # Tell them they can't get it
-                print("There is no " + move[1] + " here!")
+                print('There is no ' + move[1] + ' here!')
 
-        if "item" in rooms[currentRoom] and rooms[currentRoom]["item"] == "monster":
-            print("A monster has got you... GAME OVER!")
+        if 'item' in rooms[currentRoom] and rooms[currentRoom]['item'] == 'monster':
+            print('A monster has got you... GAME OVER!')
             break
 
         # add more game play here
